d5fd2ee47e6708bf2829665b37b1d69a653c90e6
rockjoon <rockpago@gmail.com>
2021-05-04 18:57:20 +0900

    feat : 우승자 출력 기능 추가
    Cars.java
    우승자 출력 기능 추가

--- src/main/java/racing/car/Cars.java
+++ src/main/java/racing/car/Cars.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static racing.io.ConsoleMessage.RACING_WINNERS;
+import static racing.io.Output.print;
+
 public class Cars {
 
     private final List<Car> cars;
@@ -42,4 +45,20 @@ public class Cars {
         return cars;
     }
 
+    public void printWinner(List<Car> winners) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < winners.size(); i++) {
+            appendWinner(sb, winners.get(i), i, winners.size());
+        }
+        sb.append(RACING_WINNERS.message());
+        print(sb.toString());
+    }
+
+    private void appendWinner(StringBuilder sb, Car winner, int index, int size) {
+        if (size - 1 == index) {
+            sb.append(winner.getName().getValue());
+            return;
+        }
+        sb.append(winner.getName().getValue()).append(", ");
+    }
 }
