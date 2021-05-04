64864ca8053c3983d8fd34ca0803281c884c86ec
rockjoon <rockpago@gmail.com>
2021-05-04 18:53:16 +0900

    fix : typo

--- src/main/java/racing/io/ConsoleMessage.java
+++ src/main/java/racing/io/ConsoleMessage.java
@@ -6,7 +6,7 @@ public enum ConsoleMessage {
     REQUEST_REPEAT("시도할 횟수는 몇번인가요"),
     RACING_RESULT("실행 결과"),
     RACING_WINNERS("가 최종우승했습니다."),
-    SCORE_SEPERATOR(":"),
+    SCORE_SEPARATOR(":"),
     RACING_DISTANCE("-"),
     REQUEST_ERROR("잘못된 입력입니다. 다시 한번 확인해 주세요.");
 
